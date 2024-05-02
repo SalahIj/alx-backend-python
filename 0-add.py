8bddeec0e0c8213b2d0fd6ff76ae304dc48a8a1c
Ijbar Salah Eddine <ijbarsalaheddine@gmail.com>
2024-05-02 07:21:37 +0000

    Task_0

---
+++ 0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Imported modules
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Funstion definition
+    Args:
+        a: first input
+        b: second input
+    """
+    return a + b
