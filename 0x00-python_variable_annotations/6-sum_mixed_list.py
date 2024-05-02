ff632135df2c10bd7dc8797c1ea1b9d57f732f97
Ijbar Salah Eddine <ijbarsalaheddine@gmail.com>
2024-05-02 07:43:34 +0000

    Task_6

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Imported modules
+"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """
+    Function definition
+    Args:
+        mxd_lst: the input of the function
+    """
+    return sum(mxd_lst)
