7a538faa54df9b35a2fccee29c771fb4a9a89da3
Ijbar Salah Eddine <ijbarsalaheddine@gmail.com>
2024-05-02 07:26:43 +0000

    Task_1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Imported modules
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Function definition
+    Args:
+        str1: the first input
+        str2: the second input
+    """
+    return str1 + str2
