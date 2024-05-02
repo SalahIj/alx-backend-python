840b312b1b1a92ed9289fc36b8a80805b736260b
Ijbar Salah Eddine <ijbarsalaheddine@gmail.com>
2024-05-02 07:38:53 +0000

    Task_5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Imported modules
+"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Function definition
+    Args:
+        input_list: the input
+    """
+    return sum(input_list)
