49ceb56b239512dbae5ef903d671649e8aa97c86
Ijbar Salah Eddine <ijbarsalaheddine@gmail.com>
2024-05-06 07:54:18 +0000

    Task_1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,6 +8,18 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """ The function definition """
-    tasks = [wait_random(max_delay) for _ in range(n)]
-    return await asyncio.gather(*tasks)
+    """ The function definition
+    Args:
+        n: the first input
+        max_delay: the second input
+    """
+    delaits: List[float] = []
+    delaitsList: List[float] = []
+
+    for i in range(n):
+        delaits.append(wait_random(max_delay))
+
+    for o in asyncio.as_completed(delaits):
+        delaitsList.append(await o)
+
+    return delaitsList
