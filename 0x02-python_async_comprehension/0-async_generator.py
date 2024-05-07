7d59cb161694c2b9790f1ee91109ed477d2a538a
Ijbar Salah Eddine <ijbarsalaheddine@gmail.com>
2024-05-07 16:41:50 +0000

    Task_1

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -8,7 +8,6 @@ from typing import Generator
 async def async_generator() -> Generator[float, None, None]:
     """ Function definition
     Args:
-        
     """
     for _ in range(0, 10):
         await asyncio.sleep(1)
---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Imported modules """
+import asyncio
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """The function definition
+    Args:
+        No args.
+    """
+    return [i async for i in async_generator()]
